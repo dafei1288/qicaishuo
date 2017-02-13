9a1436c4e65b1e3cef72aac426218b6576ee4dd6
dafei1288 <dafei1288@sina.com>
2017-02-13 12:27:24 +0800

    add settings.py

--- robot.py
+++ robot.py
@@ -1,15 +1,7 @@
-import werobot
-
-robot = werobot.WeRoBot(token='qicaishuo1288')
-robot.config["APP_ID"] = "wxe879a75619613e2b"
-#robot.config["APP_SECRET"] = "你的 AppSecret"
+from settings import robot
 
 @robot.text
 def hello_world():
     return 'Hello World!'
 
 
-
-# robot.config['HOST'] = '0.0.0.0'
-# robot.config['PORT'] = 9090
-# robot.run()
---
+++ settings.py
@@ -0,0 +1,5 @@
+import werobot
+
+robot = werobot.WeRoBot(token='yours token')
+robot.config["APP_ID"] = "your app id"
+robot.config["APP_SECRET"] = "your secret"
