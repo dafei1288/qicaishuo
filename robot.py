de6089fb58b19a8154013fe295b25a3b7f512952
dafei1288 <dafei1288@sina.com>
2017-02-13 13:32:01 +0800

    chang ignore

--- robot.py
+++ robot.py
@@ -5,3 +5,17 @@ def hello_world():
     return 'Hello World!'
 
 
+client = robot.client
+
+
+client.create_menu({
+    "button":[{
+         "type": "click",
+         "name": "今日歌曲",
+         "key": "music"
+    }]
+})
+
+@robot.key_click("music")
+def music(message):
+    return '你点击了“今日歌曲”按钮'
